1bae466e539638acf1b3c0473acb7c27950ef1f2
jhyoon <ho9036@gmail.com>
2024-12-01 22:36:19 +0900

    윈도우 창 기본 바인딩 구성

---
+++ WPF.Sample/Converters/HexColorCodeToSolidColorBrushConverter.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace WPF.Sample.Converters
+{
+    public class HexColorCodeToSolidColorBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                if (value is string hexColorCode)
+                {
+                    return new SolidColorBrush((Color)ColorConverter.ConvertFromString(hexColorCode));
+                }
+            }
+            catch (Exception) { }
+
+            return new SolidColorBrush(Colors.Transparent);
+        }
+
+        public object? ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
--- WPF.Sample/ViewModels/MainWindowViewModel.cs
+++ WPF.Sample/ViewModels/MainWindowViewModel.cs
@@ -4,5 +4,36 @@ namespace WPF.Sample.ViewModels
 {
     public class MainWindowViewModel : PropertyChangeBase
     {
+        private string _windowTitle = string.Empty;
+
+        private double _windowWidth = 0;
+
+        private double _windowHeight = 0;
+
+        private string _windowBakcground = "#00000000";
+
+        public string WindowTitle
+        {
+            get => _windowTitle;
+            set => SetProperty(ref _windowTitle, value);
+        }
+
+        public double WindowWidth
+        {
+            get => _windowWidth;
+            set => SetProperty(ref _windowWidth, value);
+        }
+
+        public double WindowHeight
+        {
+            get => _windowHeight;
+            set => SetProperty(ref _windowHeight, value);
+        }
+
+        public string WindowBackground
+        {
+            get => _windowBakcground;
+            set => SetProperty(ref _windowBakcground, value);
+        }
     }
 }
