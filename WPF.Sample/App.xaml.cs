f70a8b29c80aeb9e22d4a85ef1078fe52208f7e4
jhyoon <ho9036@gmail.com>
2024-12-01 11:41:37 +0900

    MainWindow의 싱글톤

--- WPF.Sample/App.xaml.cs
+++ WPF.Sample/App.xaml.cs
@@ -1,6 +1,7 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System.Windows;
+using WPF.Sample.ViewModels;
 
 namespace WPF.Sample
 {
@@ -9,6 +10,36 @@ namespace WPF.Sample
     /// </summary>
     public partial class App : Application
     {
-    }
+        private IHost _host;
 
+        public App()
+        {
+            InitializeComponent();
+
+            _host = Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddSingleton<MainWindow>();
+                    services.AddSingleton<MainWindowViewModel>();
+                })
+                .Build();
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            await _host.StartAsync();
+            var mainWindow = _host.Services.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+            base.OnStartup(e);
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            using (_host)
+            {
+                await _host.StopAsync();
+            }
+            base.OnExit(e);
+        }
+    }
 }
---
+++ WPF.Sample/Models/Base/PropertyChangeBase.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WPF.Sample.Models.Base
+{
+    public class PropertyChangeBase : INotifyPropertyChanged, INotifyPropertyChanging
+    {
+        public event PropertyChangingEventHandler? PropertyChanging;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void SetProperty<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+        {
+            if (!Equals(field, value))
+            {
+                OnPropertyChanging(propertyName);
+                field = value;
+                OnPropertyChanged(propertyName);
+            }
+        }
+
+        protected void OnPropertyChanging([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanging?.Invoke(this, new PropertyChangingEventArgs(propertyName));
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
